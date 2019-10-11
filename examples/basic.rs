749a312438b5d0344a1a098135655a1fd96aba22
Roberto <rob.ariosa@hotmail.com>
2019-10-11 19:20:17 +0100

    New example

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,27 +1,25 @@
-
 extern crate gym;
 use gym::GymClient;
 
 fn main() {
-	let client = GymClient::new_quiet("http://localhost:5000".to_string()).unwrap();
+	let client = GymClient::new("127.0.0.1".parse().unwrap(), 8000).unwrap();
 	println!("Already running environments:\n{:?}\n", client.get_envs().unwrap());
 
-	let env = match client.make("CartPole-v0") {
-		Ok(env) => env,
-		Err(msg) => panic!("Could not make environment because of error:\n{}", msg)
-	};
+	let env = client
+		.make("CartPole-v0".into(), None)
+		.expect("Could not make environment");
 
 	println!("Observation space:\n{:?}\n", env.observation_space());
 	println!("Action space:\n{:?}\n", env.action_space());
 
-	let _ = env.monitor_start("/tmp/random-agent-results".to_string(), true, false);
-
 	for ep in 0..10 {
 		let mut tot_reward = 0.;
 		let _ = env.reset();
+
 		loop {
 			let action = env.action_space().sample();
 			let state = env.step(action, true).unwrap();
+
 			assert_eq!(state.observation.len(), env.observation_space().sample().len());
 			tot_reward += state.reward;
 
@@ -29,8 +27,7 @@ fn main() {
 				break;
 			}
 		}
+
 		println!("Finished episode {} with total reward {}", ep, tot_reward);
 	}
-
-	let _ = env.monitor_stop();
 }
